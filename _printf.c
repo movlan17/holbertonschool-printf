7f967085f0ffe0fbe1c07d8eafb187860286b9ea
movlan17 <movlanomerov00@gmail.com>
2025-08-25 12:20:13 +0000

    guh

---
+++ _printf.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+
+/* ... _putchar, _puts kimi köməkçi funksiyalar ... */
+
+/**
+ * _printf - produces output according to a format.
+ * @format: format string.
+ *
+ * Return: number of characters printed.
+ */
+int _printf(const char *format, ...)
+{
+    /* implementasiya bura */
+}
+
