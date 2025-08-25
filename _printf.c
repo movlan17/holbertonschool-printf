ac6cde252f7f7bfb810a2ea730948433ade73123
movlan17 <movlanomerov00@gmail.com>
2025-08-25 12:29:05 +0000

    vhbvh

--- _printf.c
+++ _printf.c
@@ -1,17 +1,51 @@
 #include "main.h"
-#include <stdarg.h>
-#include <unistd.h>
-
-/* ... _putchar, _puts kimi köməkçi funksiyalar ... */
 
 /**
- * _printf - produces output according to a format.
- * @format: format string.
- *
- * Return: number of characters printed.
+ * _printf - print function
+ * @format: str
+ * Return: integer
  */
+
 int _printf(const char *format, ...)
 {
-    /* implementasiya bura */
-}
+	char *buff;
+	int  i = 0, count = 0;
+	va_list arg_value;
+	int (*func)(char *, int, va_list);
 
+	buff = malloc(4000);
+	if ((!format || !buff) || (format[0] == '%' && format[1] == '\0'))
+	{
+		free(buff);
+		exit(1);
+	}
+
+	va_start(arg_value, format);
+
+	while (format[i])
+	{
+		if (format[i] != '%')
+		{
+			buff[count] = format[i];
+			count++;
+		}
+		else
+		{
+			func = check_prtr(format[i + 1]);
+			if (!func)
+			{
+				buff[count] = '%';
+				i++;
+				count++;
+				continue;
+			}
+			count = func(&buff[count], count, arg_value);
+			i++;
+		}
+		i++;
+	}
+	write(1, buff, count);
+	va_end(arg_value);
+	free(buff);
+	return (count);
+}
